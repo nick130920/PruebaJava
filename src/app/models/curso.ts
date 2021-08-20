8152238c11e37ec220c7e15b45d28755ce24119e
nick130920 <namc1309@gmail.com>
2021-08-20 13:54:02 -0500

    eliminar

--- src/app/models/curso.ts
+++
@@ -1,6 +0,0 @@
-export class Curso {
-  id?: number;
-  code: string;
-  description: string;
-  name: string;
-}
--- src/app/models/salon.ts
+++
@@ -1,6 +0,0 @@
-export class Salon {
-  id?: number;
-  aforo: number;
-  description: string;
-  name: string;
-}
